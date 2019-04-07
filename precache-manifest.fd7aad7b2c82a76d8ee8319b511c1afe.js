4f8ccc6e35bc3e13f08c2d9ccf735908b8c7f8c3
Kuba <jakzacha@gmail.com>
2019-04-07 22:33:24 +0200

    Updates

---
+++ precache-manifest.fd7aad7b2c82a76d8ee8319b511c1afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "caf7e8b19f70f58562cc",
+    "url": "/FontAwesomeMemoryGame/static/css/main.7513d66d.chunk.css"
+  },
+  {
+    "revision": "caf7e8b19f70f58562cc",
+    "url": "/FontAwesomeMemoryGame/static/js/main.56c9acf0.chunk.js"
+  },
+  {
+    "revision": "54a7990cfe9d2906a1cc",
+    "url": "/FontAwesomeMemoryGame/static/js/runtime~main.a74b1aeb.js"
+  },
+  {
+    "revision": "9d1a8246191912440b8c",
+    "url": "/FontAwesomeMemoryGame/static/js/2.395498ee.chunk.js"
+  },
+  {
+    "revision": "ae87ad37307ce14dde6758f44ac8c657",
+    "url": "/FontAwesomeMemoryGame/static/media/background3.ae87ad37.svg"
+  },
+  {
+    "revision": "bd3ad7144336353979a662fa9924ff9b",
+    "url": "/FontAwesomeMemoryGame/index.html"
+  }
+];
